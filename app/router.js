87e567df9fd42bceb8f4f4ef967047584028dbc1
Rishant Dwivedi <Rishant Dwivedi>
2015-10-24 18:11:29 -0500

    Auth

--- app/router.js
+++ app/router.js
@@ -2,8 +2,9 @@ var controller = require('./controllers')
 var middleware = require('./middleware')
 var services = require('./services')
 
+
 services.manager.sync()
 
 module.exports = function(app) {
-
+	app.get('/api/app/login', middleware.auth.isLoggedIn) 
 }
