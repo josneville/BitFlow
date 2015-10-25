33ad8721850ad0ce9a0427d44dfd1f7d99dd45ba
Neville Jos <josneville@yahoo.com>
2015-10-25 09:16:17 -0700

    create stripe before anything

--- app/controllers/transactions.js
+++ app/controllers/transactions.js
@@ -1,53 +1,58 @@
 var config = require('../../config')
-var knex = require('knex')({client: 'postgresql', connection: config.postgres})
+var knex = require('knex')({
+	client: 'postgresql',
+	connection: config.postgres
+})
+var stripe = require('stripe')(config.stripe)
 
 var getBalance = require('../services/balance')
 var sendMoney = require('../services/sendMoney')
 
 var coinbase = require('../services/coinbase')
 
-module.exports= {
-  get: function(req, res, next){
-    var user = res.locals.user
+module.exports = {
+	get: function (req, res, next) {
+		var user = res.locals.user
 
-    knex('transactions')
-    	.join('users', function(){
-        this.on('transactions.from_id', '=', 'users.id').orOn('transactions.to_id', '=', 'users.id')
-      })
-      .select('users.facebook_id', 'users.picture', 'users.name', 'transactions.amount', 'transactions.message')
-    	.then(function(rows){
-    		res.status(200).send({transactions: rows})
-    	})
-    	.catch(function(err){
-    		next(err)
-    	})
-  },
-  create: function(req, res, next){
-    var user = res.locals.user
-    var toUser = res.locals.toUser
+		knex('transactions')
+			.join('users', function () {
+				this.on('transactions.from_id', '=', 'users.id').orOn('transactions.to_id', '=', 'users.id')
+			})
+			.select('users.facebook_id', 'users.picture', 'users.name', 'transactions.amount', 'transactions.message')
+			.then(function (rows) {
+				res.status(200).send({
+					transactions: rows
+				})
+			})
+			.catch(function (err) {
+				next(err)
+			})
+	},
+	create: function (req, res, next) {
+		var user = res.locals.user
+		var toUser = res.locals.toUser
 
-    console.log(req.body)
-    var amount = req.body.amount
-    var password = config.password //req.body.password
-    var message = req.body.message
-
-    console.log("Before send Money")
-    sendMoney(user, password, toUser, amount, message, function(err, response){
-      console.log("Hi 1")
-      if (err) return next(err)
-      console.log("Hi 2")
-      if (response.error){
-        console.log("Hi 3")
-        coinbase.buyAndTransfer(amount, toUser, function(err){
-          console.log("Hi 4")
-          if (err) return next(err)
-          return res.status(200).send({})
-        })
-      }
-      else {
-        console.log("hi 5")
-        res.status(200).send({})
-      }
-    })
-  }
+		var amount = req.body.amount
+		var password = config.password //req.body.password
+		var message = req.body.message
+		var user = res.locals.user
+		stripe.charges.create({
+			amount: amount * 100, // amount in cents, again
+			currency: "usd",
+			source: user.stripe_customer_id
+		}, function (err, charge) {
+			if (err) return cb(err)
+			sendMoney(user, password, toUser, amount, message, function (err, response) {
+				if (err) return next(err)
+				if (response.error) {
+					coinbase.buyAndTransfer(amount, toUser, function (err) {
+						if (err) return next(err)
+						return res.status(200).send({})
+					})
+				} else {
+					res.status(200).send({})
+				}
+			})
+		})
+	}
 }
--- app/services/coinbase.js
+++ app/services/coinbase.js
@@ -2,36 +2,31 @@ var config = require('../../config')
 var stripe = require('stripe')(config.stripe)
 
 var Client = require('coinbase').Client;
-var client = new Client({'apiKey': config.coinbase.key, 'apiSecret': config.coinbase.secret})
+var client = new Client({
+	'apiKey': config.coinbase.key,
+	'apiSecret': config.coinbase.secret
+})
 
 module.exports = {
-  buyAndTransfer: function(amount, toUser, cb){
-    //charge the current card with stripe
-    var user = res.locals.user
-    stripe.charges.create({
-      amount: amount * 100, // amount in cents, again
-      currency: "usd",
-      source: user.stripe_customer_id
-    }, function(err, charge) {
-      if (err) return cb(err)
-      client.getAccounts({}, function(err, accounts){
-        client.getAccount(accounts[0].id, function(err, account){
-          account.buy({
-            amount: amount,
-            currency: "USD"
-          }, function(err, xfer){
-          // if (err) return cb(err)
-            account.sendMoney({
-              to: toUser.bitcoin_address,
-              amount: amount,
-              currency: "USD"
-            }, function(err, txn){
-              if (err) return cb(err)
-              return cb()
-            })
-          })
-        })
-      })
-    })
-  }
+	buyAndTransfer: function (amount, toUser, cb) {
+		//charge the current card with stripe
+		client.getAccounts({}, function (err, accounts) {
+			client.getAccount(accounts[0].id, function (err, account) {
+				account.buy({
+					amount: amount,
+					currency: "USD"
+				}, function (err, xfer) {
+					// if (err) return cb(err)
+					account.sendMoney({
+						to: toUser.bitcoin_address,
+						amount: amount,
+						currency: "USD"
+					}, function (err, txn) {
+						if (err) return cb(err)
+						return cb()
+					})
+				})
+			})
+		})
+	}
 }
