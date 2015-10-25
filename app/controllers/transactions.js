097b7890393809ddbf54bee61914e4ff1babd3b6
Rishant Dwivedi <Rishant Dwivedi>
2015-10-24 23:16:56 -0500

    Transacations and Balances

--- app/controllers/transactions.js
+++ app/controllers/transactions.js
@@ -1,5 +1,22 @@
+var config = require('../../config')
+var knex = require('knex')({client: 'postgresql', connection: config.postgres})
+
 module.exports= {
   get: function(req, res, next){
     var user = res.locals.user
+
+    knex('transactions')
+    	.where({
+    		from_id: user.id
+    	})
+    	.orWhere({
+    		to_id: user.id
+    	})
+    	.then(function(rows){
+    		res.status(200).send({transactions: rows})
+    	})
+    	.catch(function(err){
+    		next(err)	
+    	}) 
   }
 }
--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,10 +1,23 @@
 var config = require('../../config')
+var request = require('superagent')
+var blockchain = require('blockchain.info')
 
 var knex = require('knex')({client: 'postgresql', connection: config.postgres})
-var blockchain = require('blockchain.info')
 
 module.exports = {
   balance: function(req, res, next){
 
+  	var user = res.locals.user
+  	var pass = req.body.password
+
+  	request
+		.get('https://blockchain.info/merchant/' + user.blockchain_wallet + '/balance?password=' + pass)
+		.end(function(err, response){
+			if (err) {
+				next(err)
+			}
+
+			res.status(200).send({balance: response.balance})
+		})
   }
 }
