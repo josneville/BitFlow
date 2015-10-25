aa4aaa5312f2746477edd533b55116c17691f181
Neville Jos <josneville@yahoo.com>
2015-10-25 09:52:31 -0700

    picture location updated

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -23,7 +23,7 @@ module.exports = {
 							facebook_id: body.id,
 							email: body.email,
 							name: body.name,
-							picture: JSON.parse(body.picture).url
+							picture: JSON.parse(body.picture).data.url
 						})
 					}
 					success.rows = rows
