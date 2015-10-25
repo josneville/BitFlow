72c18954a24822ac387dd5a94c17b116506c3637
Neville Jos <josneville@yahoo.com>
2015-10-24 21:32:19 -0700

    users model updated

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -2,9 +2,48 @@ var config = require('../../config')
 
 var knex = require('knex')({client: 'postgresql', connection: config.postgres})
 var blockchain = require('blockchain.info')
+var stripe = require('stripe')(config.stripe)
 
 module.exports = {
   balance: function(req, res, next){
 
+  },
+  account: function(req, res, next){
+    var user = res.locals.user
+    var card = req.body.card
+    var password = req.body.password
+
+    stripe.accounts.create({
+      managed: true,
+      country: 'US',
+      email: user.email
+    }, function(err, account){
+      if (err) return next(err)
+      stripe.accounts.createExternalAcccount(account.id, {
+        external_account: card,
+        default_for_currency: true
+      }, function(err, bank_account){
+        if (err) return next(err)
+        var newWallet = new blockchain.CreateWallet(password, config.blockchain)
+        newWallet.create(function(err, wallet){
+          if (err) return next(err)
+          knex('users')
+            .where({
+              id: user.id
+            })
+            .update({
+              blockchain_wallet: wallet.guid,
+              stripe_account: account.id,
+              stripe_key: account.keys.secret
+            })
+            .then(function(){
+              res.status(200).send({})
+            })
+            .catch(function(err){
+              next(err)
+            })
+        })
+      })
+    })
   }
 }
--- app/router.js
+++ app/router.js
@@ -3,7 +3,7 @@ var middleware = require('./middleware')
 
 module.exports = function(app) {
 	app.get('/users/balance', middleware.auth.isLoggedIn, controllers.users.balance)
-	app.put('/users/account', middleware.auth.isLoggedIn, controllers.users.bank)
+	app.put('/users/account', middleware.auth.isLoggedIn, controllers.users.account)
 
 	app.get('/transactions', middleware.auth.isLoggedIn, controllers.transactions.get)
 }
